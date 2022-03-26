d24b67d9069ac61703f3a69c1d9f5f75666d02cc
dgsoftwarehub <dginfotechpvtltd@gmail.com>
2022-03-26 09:40:39 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily:{
+        body:['Montserrat','Arial']
+      },
+      colors:{
+        'topvote-purple':'#432353',
+        'topvote-light-purple':'#6b4678',
+        'topvote-light-grey':'#f2f2f2',
+        'topvote-gold':'#f5cd44',
+        'topvote-green':'#1dbb9c',
+        // 'topvote-red':'',
+        // 'topvote-orange':'',
+        // 'topvote-yellow':'',
+        'topvote-overlay':'rgba(47, 53, 62, 0.86)',
+        'facebook-blue':'#405792',
+        'reddit-blue':'#5f99cf',
+        'twitter-blue':'#00abed',
+        'instagram-purple':'#bc438b'
+      },
+      inset:{
+        '-8':'-2rem'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
